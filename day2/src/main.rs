82e3a270bda1e11ab117cb29adec22956e83a750
Beatriz <beatrizmaecruz@proton.me>
2025-12-10 01:01:50 +1300

    Added plans for day2 in comments + .gitignore file

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // PART 1: File proccessing.
+    // Process file into long string.
+    // Separate by comma (into Vec<String> iterable) into ID ranges.
+    // Separate each range by dash eg: XX-YY into "XX" and "YY".
+
+    // PART 2: Processing each ID.
+    // Initalize a sum of 0.
+    // Iterate through each range, getting numbers from XX to YY.
+    // Check if the numbers has repeating patterns. <- will be a function in itself (does_number_have_pattern, using Regex maybe?)
+    // If return true, then add the number to sum.
+
+    println!("Hello day 2");
 }
